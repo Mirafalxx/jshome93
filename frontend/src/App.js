1d3c7a6e13f2d9abec04b2ca817086d6c2e8cb50
Mirafalxx <maxi.suhanov@gmail.com>
2021-05-18 15:11:06 +0600

    added login # NEED TO FIX

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,21 @@
-import React from 'react';
-import { Route, Switch } from 'react-router';
-import AppToolbar from './Components/UI/AppToolbar/AppToolbar';
-import Register from './Containers/Register/Register'
-import Login from './Containers/Login/Login'
-import NewEvent from './Containers/NewEvent/NewEvent'
-import Layout from './Components/UI/Layout/Layout'
+import React from "react";
+import { Route, Switch } from "react-router";
+import AppToolbar from "./Components/UI/AppToolbar/AppToolbar";
+import Register from "./Containers/Register/Register";
+import Login from "./Containers/Login/Login";
+import NewEvent from "./Containers/NewEvent/NewEvent";
+import Layout from "./Components/UI/Layout/Layout";
 
-
-
-
-const App=()=> {
+const App = () => {
   return (
     <Layout>
       <Switch>
-        <Route path='/' component={NewEvent}/>
-        <Route path='/register' component={Register}/>
-        <Route path='/login' component={Login}/>
+        <Route path="/" exact component={NewEvent} />
+        <Route path="/register" component={Register} />
+        <Route path="/login" component={Login} />
       </Switch>
     </Layout>
   );
-}
+};
 
 export default App;
--- frontend/src/Containers/Register/Register.js
+++ frontend/src/Containers/Register/Register.js
@@ -1,11 +1,90 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link as RouterLink } from "react-router-dom";
+import { Avatar, Button, Container, Grid, Link, makeStyles, Typography } from "@material-ui/core";
+import FormElement from "../../Components/UI/Form/FormElement";
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+  header: {
+    marginBottom: theme.spacing(2),
+  },
+}));
 
 const Register = () => {
-    return (
-        <div>
-            REGISTER
-        </div>
-    )
-}
+  const classes = useStyles();
+  return (
+    <Container component="section" maxWidth="xs">
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>{/* <LockOutlinedIcon /> */}</Avatar>
+        <Typography component="h1" variant="h5" className={classes.header}>
+          Sign up
+        </Typography>
+        <Grid container spacing={1} direction="column" component="form" noValidate>
+          <FormElement
+            required
+            label="Email"
+            type="text"
+            name="email"
+            autoComplete="new-email"
+            // error={getFieldError("email")}
+          />
+          <FormElement
+            required
+            label="Password"
+            type="password"
+            name="password"
+            autoComplete="new-password"
+            // error={getFieldError("password")}
+          />
+          <FormElement
+            required
+            label="Display Name"
+            type="text"
+            name="displayName"
+
+            // error={getFieldError("displayName")}
+          />
+          <Grid item xs>
+            <Button>Sign up</Button>
+            {/* <ButtonWithProgress
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+              loading={loading}
+              disabled={loading}
+            >
+              Sign up
+            </ButtonWithProgress> */}
+          </Grid>
+          <Grid item container justify="flex-end">
+            <Grid item>
+              <Link component={RouterLink} variant="body2" to="/login">
+                Already have an account? Sign in
+              </Link>
+            </Grid>
+          </Grid>
+        </Grid>
+      </div>
+    </Container>
+  );
+};
 
 export default Register;
